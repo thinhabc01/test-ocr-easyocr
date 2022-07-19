070dc59c67baf9a1a9b0abd673960872b066ea11
thinhabc01 <62796914+thinhabc01@users.noreply.github.com>
2022-07-19 23:07:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JSON_SORT_KEYS'] = False
-
+reader = None
 
 def solutionCaptcha(captcha):
-    reader = easyocr.Reader(['en'], gpu=False)
+    if reader == None:
+        reader = easyocr.Reader(['en'], gpu=False)
     result = reader.readtext(captcha, detail=0)
     return result
 
